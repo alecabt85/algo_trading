92d134079f6ad228a1168017841bc97e983d4318
alecabt85 <23139300+alecabt85@users.noreply.github.com>
2024-01-05 18:04:56 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='First pass at using ML for actively managed assets',
+    author='Alec Burtner-Abt',
+    license='',
+)
